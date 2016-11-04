0ae84874a675d3d74714c629890ddcab7205da38
matthieudht <matthieu.dehondt@gmail.com>
2016-11-04 15:48:33 +0100

    Update connexion-sql.php

--- modele/connexion-sql.php
+++ modele/connexion-sql.php
@@ -3,7 +3,7 @@
 //connexion to database
 
 try {
-    $myBase = new PDO('mysql:host=localhost;dbname=exercice;charset=utf8','dehondtmatthieu', 'mD120989');
+    $myBase = new PDO('mysql:host=localhost;dbname=exercice;charset=utf8',');
 }
 catch (Exception $error) {
     die('Erreur: '.$error->getMessage());
